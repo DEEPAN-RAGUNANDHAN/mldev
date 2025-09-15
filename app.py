b707dcd6290166456df1904e768b624955b3be7f
deep <shoot2deepan@gmail.com>
2025-09-16 01:05:55 +0530

    Add binary, Dockerfile, and push to GitHub registry

--- app.py
+++ app.py
@@ -434,6 +434,7 @@ async def external_job_api(req: ExternalJobRequest, _: None = Depends(verify_tok
             "match_score": match_score
         })
 
+
     except HTTPException as http_err:
         raise http_err
     except Exception as e:
